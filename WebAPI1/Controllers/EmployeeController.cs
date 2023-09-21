0e6ab69cacec4d22c4d99f9f97a880927a84b392
ChiragGavs <chirag.rohilla@gavstech.com>
2023-09-21 16:53:33 +0530

    Employee listed

---
+++ WebAPI1/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+using WebAPI1.Models;
+
+namespace WebAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmpLoyeeRepository _context;
+        public EmployeeController(EmpLoyeeRepository context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/api/AllEmpoyee")]
+        public List<Employee> AllEmployee()
+        {
+            return _context.GetEmployees();
+        }    
+    }
+}
---
+++ WebAPI1/Models/EmpLoyeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace WebAPI1.Models
+{
+    public class EmpLoyeeRepository
+    {
+        private NorthwindContext _context;
+         public EmpLoyeeRepository (NorthwindContext context)
+        {
+            _context = context;
+        }
+       public List<Employee> GetEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+    }
+}
